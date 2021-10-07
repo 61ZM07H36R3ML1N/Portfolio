69e2139471db8d8cbdd55b0a14e371412593fbfd
BT <brianthomasjamiel@gmaail.com>
2021-10-07 14:41:42 -0700

    added const data for greeting

--- routes/main.js
+++ routes/main.js
@@ -2,8 +2,13 @@ const vertex = require('vertex360')({site_id: process.env.TURBO_APP_ID})
 const router = vertex.router()
 
 router.get('/', (req, res) => {
-
-    res.render('landing')
+    const data = {
+        greeting: 'Hello, Welcome to my Portfolio!'
+    }
+    
+    
+    
+    res.render('landing', data)
 
 })
 
