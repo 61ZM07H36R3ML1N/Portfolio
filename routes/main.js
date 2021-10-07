44d23e248dc12f6783a8af71fe84addace13608c
BT <brianthomasjamiel@gmaail.com>
2021-10-07 15:53:42 -0700

    added image of self

--- routes/main.js
+++ routes/main.js
@@ -3,8 +3,17 @@ const router = vertex.router()
 
 router.get('/', (req, res) => {
     const data = {
+        image_profile: 'https://photos.google.com/photo/AF1QipNhxLlY69_fN_wpPiZzYxahxumABm_oxI7HHk7m',
         greeting: 'Hello, Welcome to my Portfolio!',
-        introduction: 'I am a self taught junior web developer from the Puget Sound area.'
+        introduction: 'I am a self taught junior web developer from the Puget Sound area.',
+        languages: [
+            {name:'Javascript', years:1},
+            {name:'Node.js', years:1},
+            {name:'Express', years:1},
+            {name:'HTML5', years:1},
+            {name:'Mustache', years:1},
+            {name:'CSS', years:1},
+        ]
     }
     
     
