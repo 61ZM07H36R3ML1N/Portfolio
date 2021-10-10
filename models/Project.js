2e25835f1ec882c78f26ffb2d60161d4a8a54e0a
BT <brianthomasjamiel@gmaail.com>
2021-10-09 21:07:38 -0700

    just beautified it

--- models/Project.js
+++ models/Project.js
@@ -11,22 +11,65 @@
 // https://github.com/scottwrobinson/camo
 const Document = require('vertex-camo').Document
 const props = {
-  image: {type:String, default:''},
-  name: {type:String, default:'', trim:true, display:true},
-  category: {type:String, default:''},
-	slug: {type:String, default:'', immutable:true},
-  preview: {type:String, default:''},
-  link: {type:String, default:''},
-  client: {type:String, default:''},
-  dateRange: {type:String, default:''},
-  description: {type:String, default:'', isHtml:true},
-	schema: {type:String, default:'project', immutable:true},
-	dateString: {type:String, default:'', immutable:true},
-	timestamp: {type:Date, default: new Date(), immutable:true}
+	image: {
+		type: String,
+		default: ''
+	},
+	name: {
+		type: String,
+		default: '',
+		trim: true,
+		display: true
+	},
+	category: {
+		type: String,
+		default: ''
+	},
+	slug: {
+		type: String,
+		default: '',
+		immutable: true
+	},
+	preview: {
+		type: String,
+		default: ''
+	},
+	link: {
+		type: String,
+		default: ''
+	},
+	client: {
+		type: String,
+		default: ''
+	},
+	dateRange: {
+		type: String,
+		default: ''
+	},
+	description: {
+		type: String,
+		default: '',
+		isHtml: true
+	},
+	schema: {
+		type: String,
+		default: 'project',
+		immutable: true
+	},
+	dateString: {
+		type: String,
+		default: '',
+		immutable: true
+	},
+	timestamp: {
+		type: Date,
+		default: new Date(),
+		immutable: true
+	}
 }
 
 class Project extends Document {
-	constructor(){
+	constructor() {
 		super()
 		this.schema(props)
 
@@ -34,12 +77,12 @@ class Project extends Document {
 		this.timestamp = new Date()
 	}
 
-	static get resourceName(){
+	static get resourceName() {
 		return 'project'
 	}
 
-	static collectionName(){
-			return 'projects'
+	static collectionName() {
+		return 'projects'
 	}
 }
 
