1500006668e8ca62e1114fe9bd1bf0170496ac05
Brian Jamiel <brianthomasjamiel@gmail.com>
2026-03-01 09:46:55 -0800

    added ProjectSection

--- brianjamiel-portfolio/src/app/page.tsx
+++ brianjamiel-portfolio/src/app/page.tsx
@@ -1,9 +1,12 @@
+// src/app/page.tsx
 import Hero from "@/components/Hero";
+import  ProjectSection  from "@/components/ProjectSection"; // This is where our Firestore cards live
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <ProjectSection />
     </main>
   );
 }
