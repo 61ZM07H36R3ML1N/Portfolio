be7ec2480238682a79e2428a345550e1d989f1f4
61ZM07H36R3ML1N <brianthomasjamiel@gmail.com>
2026-03-05 16:00:24 -0800

    exporting Architects

--- src/types/index.ts
+++ src/types/index.ts
@@ -8,3 +8,13 @@ export interface Project {
   dice_system?: string;
   tags: string[];
 }
+
+export interface Architect {
+  id: string;
+  name: string;
+  role: string;
+  bio: string;
+  github?: string;
+  linkedin?: string;
+  avatar_url?: string;
+}
